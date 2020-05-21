b80da534290e68d49187894952e4347ae4086900
David Kunkler <dkunkler@github.com>
2020-05-20 19:49:42 -0700

    Fnished Lambda buttons

--- src/_00_JButtons_with_Lambdas/LambdaButtons.java
+++ src/_00_JButtons_with_Lambdas/LambdaButtons.java
@@ -1,9 +1,12 @@
 package _00_JButtons_with_Lambdas;
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class LambdaButtons {
 	private JFrame window = new JFrame();
@@ -20,6 +23,40 @@ public class LambdaButtons {
 		
 		//1. Call the addActionListener methods for each button. Use lambdas
 		//   to define to functionality of the buttons.
+		
+		addNumbers.addActionListener(
+		
+		(ActionEvent e)->{
+			String n1 = JOptionPane.showInputDialog("enter your first number");
+			String n2 = JOptionPane.showInputDialog("Enter your second numner");
+			int sum = Integer.parseInt(n1) + Integer.parseInt(n2);
+			JOptionPane.showMessageDialog(null, "The sum of " + n1 + " and " + n2 + " is " + sum);
+		
+		}
+		);
+		
+		
+		randNumber.addActionListener(
+				
+				(ActionEvent e)->{
+					String n1 = JOptionPane.showInputDialog("I will pick a number between 1 and what?");
+					Random rand = new Random();
+					int i = rand.nextInt(Integer.parseInt(n1));
+					JOptionPane.showMessageDialog(null, "I pick " + i);
+				
+				}
+				);
+				
+		tellAJoke.addActionListener(
+				
+				(ActionEvent e)->{
+					JOptionPane.showMessageDialog(null, "There are only 10 types of people in this world. \nThose that know binary, and those that don't");
+				
+				}
+				);
+				
+					
+		
 		
 		window.setVisible(true);
 		window.pack();
